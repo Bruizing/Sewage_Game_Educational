501421575478dad2209d9498f3164531e98d31d2
Angel Rodriguez <122031673+Toxicxeno246@users.noreply.github.com>
2026-06-06 08:18:44 -0400

    Adjusments

--- Assets/Angel/Scripts/SceneChange.cs
+++ Assets/Angel/Scripts/SceneChange.cs
@@ -6,17 +6,17 @@ using UnityEngine.SceneManagement;
 
 public class SceneChange : MonoBehaviour
 {
-    [SerializeField] private SceneAsset sceneToLoad;
+    [SerializeField] private string sceneToLoad;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.CompareTag("Player"))
         {
-            SceneManager.LoadScene(sceneToLoad.name);
+            SceneManager.LoadScene(sceneToLoad);
         }
     }
 
     public void OnButtonClicked()
     {
-        SceneManager.LoadScene(sceneToLoad.name);
+        SceneManager.LoadScene(sceneToLoad);
     }
 }
